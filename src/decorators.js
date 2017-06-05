5a854835def3b42af51c18eb002768df0df2f982
Murdock <Alasdair.Murdock@experian.com>
2017-06-05 14:00:22 +0100

    initial prototype commit

--- src/decorators.js
+++ src/decorators.js
@@ -1,48 +1,52 @@
-const PREFIX = '$$route_'
+const ROUTES = '$$routes';
 
-function destruct(args) {
-  const hasPath = typeof args[0] === 'string'
-  const path = hasPath ? args[0] : ''
-  const middleware = hasPath ? args.slice(1) : args
+const destruct = args => {
+  const hasPath = typeof args[0] === 'string' || Object.prototype.toString.call(args[0]) === '[object RegExp]';
+  const path = hasPath ? args[0] : '';
+  const middleware = hasPath ? args.slice(1) : args;
 
   if (middleware.some(m => typeof m !== 'function')) {
     throw new Error('Middleware must be function')
   }
 
   return [path, middleware]
-}
+};
 
 // @route(method, path: optional, ...middleware: optional)
-export function route(method, ...args) {
+export const route = (method, ...args) => {
   if (typeof method !== 'string') {
     throw new Error('The first argument must be an HTTP method')
   }
 
-  const [path, middleware] = destruct(args)
+  const [path, middleware] = destruct(args);
 
-  return function (target, name) {
-    target[`${PREFIX}${name}`] = {method, path, middleware}
+  return (target, fnName) => {
+    const routes = target[ROUTES] || [];
+    target[ROUTES] = [...routes, {method, path, middleware, fnName}];
   }
-}
+};
 
 // @[method](...args) === @route(method, ...args)
-const methods = ['head', 'options', 'get', 'post', 'put', 'patch', 'del', 'delete', 'all']
-methods.forEach(method => exports[method] = route.bind(null, method))
+const methods = ['head', 'options', 'get', 'post', 'put', 'patch', 'del', 'delete', 'all'];
+methods.forEach(method => exports[method] = route.bind(null, method));
 
 // @controller(path: optional, ...middleware: optional)
-export function controller(...args) {
-  const [ctrlPath, ctrlMiddleware] = destruct(args)
+export const controller = (...args) => {
+  const [path, middleware] = destruct(args);
 
-  return function (target) {
-    const proto = target.prototype
-    proto.$routes = Object.getOwnPropertyNames(proto)
-      .filter(prop => prop.indexOf(PREFIX) === 0)
-      .map(prop => {
-        const {method, path, middleware: actionMiddleware} = proto[prop]
-        const url = `${ctrlPath}${path}`
-        const middleware = ctrlMiddleware.concat(actionMiddleware)
-        const fnName = prop.substring(PREFIX.length)
-        return {method: method === 'del' ? 'delete' : method, url, middleware, fnName}
-      })
-  }
-}
+  return target => {
+    const _routes = target.prototype[ROUTES] || [];
+    const routes = _routes.map(({method, path, middleware, fnName}) => ({
+      method: method === 'del' ? 'delete' : method,
+      path,
+      middleware,
+      fnName
+    }));
+
+    target.prototype.$controller = {
+      path,
+      middleware,
+      routes
+    };
+  };
+};
